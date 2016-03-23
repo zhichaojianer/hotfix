61af430561a3edeead46e7e42cb2794d1eeff5ad
liuchao <liuchao@imzhiliao.com>
2016-03-23 13:03:12 +0800

    debug

--- iosfix.js
+++ iosfix.js
@@ -3,15 +3,14 @@ defineClass('ZLPushAssistantViewController', {
     viewWillAppear: function(animated) {
         self.super().viewWillAppear(YES);
 
+        var backButton = PublicMethod.createNavBackButtonWithTitle(NSLocalizedString("返回", null));
+        backButton.addTarget_action_forControlEvents(self, backBarButtonClicked:, 6);
+        self.navigationItem().setLeftBarButtonItem(UIBarButtonItem.alloc().initWithCustomView(backButton));
+
         self.ui__tableView().setDelegate(self);
 
         ServiceManager.getAllChildWithClassId_Success_failure(0, block('NSArray*', function(childArray) {
 
-            if (childArray.count() * 44 > 180) {
-                self.selectChildTableHeightConstraint().setConstant(180);
-            } else {
-                self.selectChildTableHeightConstraint().setConstant(childArray.count() * 44);
-            }
         }), block(function() {
 
         }));
