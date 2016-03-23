7303f25eb3312ee62d8668e54ff1900a4a9754c2
liuchao <liuchao@imzhiliao.com>
2016-03-23 12:38:19 +0800

    debug

--- iosfix.js
+++ iosfix.js
@@ -2,24 +2,5 @@ require('PublicMethod,UIBarButtonItem,ServiceManager');
 defineClass('ZLPushAssistantViewController', {
     viewWillAppear: function(animated) {
         self.super().viewWillAppear(YES);
-
-        self.ui__tableView().setDelegate(self);
-
-
-        ServiceManager.getAllChildWithClassId_Success_failure(0, block('NSArray*', function(childArray) {
-            var index = self.childArray().indexOfObject(self.selectChildInfo());
-
-            self.setChildArray(childArray);
-            self.setSelectChildInfo(self.childArray[index]());
-            self.configSelectChild(self.selectChildInfo());
-
-            if (childArray.count() * 44 > 180) {
-                self.selectChildTableHeightConstraint().setConstant(180);
-            } else {
-                self.selectChildTableHeightConstraint().setConstant(childArray.count() * 44);
-            }
-        }), block(function() {
-
-        }));
     },
 });
