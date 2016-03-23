e04c743dbb663e7685f0a2d063e899ca988a78e3
liuchao <liuchao@imzhiliao.com>
2016-03-23 12:40:21 +0800

    debug

--- iosfix.js
+++ iosfix.js
@@ -2,5 +2,8 @@ require('PublicMethod,UIBarButtonItem,ServiceManager');
 defineClass('ZLPushAssistantViewController', {
     viewWillAppear: function(animated) {
         self.super().viewWillAppear(YES);
+
+        self.ui__tableView().setDelegate(self);
+
     },
 });
