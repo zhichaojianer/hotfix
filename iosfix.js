b6ef0d27edb56d27e170818f6fb94f38dfa66196
liuchao <liuchao@imzhiliao.com>
2016-03-23 12:43:39 +0800

    debug

--- iosfix.js
+++ iosfix.js
@@ -6,7 +6,15 @@ defineClass('ZLPushAssistantViewController', {
         self.ui__tableView().setDelegate(self);
 
         ServiceManager.getAllChildWithClassId_Success_failure(0, block('NSArray*', function(childArray) {
- 
+            var index = self.childArray().indexOfObject(self.selectChildInfo());
+
+        
+
+            if (childArray.count() * 44 > 180) {
+                self.selectChildTableHeightConstraint().setConstant(180);
+            } else {
+                self.selectChildTableHeightConstraint().setConstant(childArray.count() * 44);
+            }
         }), block(function() {
 
         }));
