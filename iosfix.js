0311551854d19639aaa0d2d58fad3a592ceaeabb
liuchao <liuchao@imzhiliao.com>
2016-03-23 12:01:01 +0800

    debug

--- iosfix.js
+++ iosfix.js
@@ -3,9 +3,9 @@ defineClass('ZLPushAssistantViewController', {
     viewWillAppear: function(animated) {
         self.super().viewWillAppear(YES);
 
-        var backButton = PublicMethod.createNavBackButtonWithTitle(NSLocalizedString("返回", null));
-        backButton.addTarget_action_forControlEvents(self, @selector(backBarButtonClicked: ), UIControlEventTouchUpInside);
-        self.navigationItem().setLeftBarButtonItem(UIBarButtonItem.alloc().initWithCustomView(backButton));
+        // var backButton = PublicMethod.createNavBackButtonWithTitle(NSLocalizedString("返回", null));
+        // backButton.addTarget_action_forControlEvents(self, @selector(backBarButtonClicked: ), UIControlEventTouchUpInside);
+        // self.navigationItem().setLeftBarButtonItem(UIBarButtonItem.alloc().initWithCustomView(backButton));
 
         self.ui_tableView().setDelegate(self);
 
