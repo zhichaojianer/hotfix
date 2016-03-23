1bda6fd57b2726ee0f661820e03b0b2dea0915fc
liuchao <liuchao@imzhiliao.com>
2016-03-23 13:25:29 +0800

    debug

--- iosfix.js
+++ iosfix.js
@@ -3,6 +3,8 @@ defineClass('ZLPushAssistantViewController', {
     viewWillAppear: function(animated) {
         self.super().viewWillAppear(YES);
 
+        self.setTitle("接送助手");
+
         var backButton = PublicMethod.createNavBackButtonWithTitle("返回");
         backButton.addTarget_action_forControlEvents(self, "backBarButtonClicked:", 6);
         self.navigationItem().setLeftBarButtonItem(UIBarButtonItem.alloc().initWithCustomView(backButton));
