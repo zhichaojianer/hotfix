53842d955567b2a50329c797c87f55745dead7e5
liuchao <liuchao@imzhiliao.com>
2016-04-02 06:41:23 +0800

    debug

--- iosfix.js
+++ iosfix.js
@@ -1,4 +1,4 @@
-require('UIColor,UIView,UILabel,UIButton,UIBarButtonItem');
+require('UIColor,UIView,UILabel,UIButton,UIImage,UIFont,UIBarButtonItem');
 defineClass('ZLVIPCenterController', {
     viewDidLoad: function() {
         self.super().viewDidLoad();
@@ -6,7 +6,7 @@ defineClass('ZLVIPCenterController', {
         self.setTitle("会员中心");
         self.view().setBackgroundColor(UIColor.whiteColor());
 
-        var backButton = UIButton.alloc().initWithFrame(self.view().frame());
+        var backButton = UIButton.alloc().initWithFrame({x:0, y:0, width:60, height:44});
         backButton.setTitle_forState("返回", 0);
         backButton.setImage_forState(UIImage.imageNamed("public_btn_back_black"), 0);
         backButton.titleLabel().setFont(UIFont.systemFontOfSize(17));
