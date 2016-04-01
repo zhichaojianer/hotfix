4ae3cd754062f8c0c100faa727d535ed0be8d9f9
liuchao <liuchao@imzhiliao.com>
2016-04-02 05:47:36 +0800

    debug

--- iosfix.js
+++ iosfix.js
@@ -1,4 +1,4 @@
-require('UIColor,UIView');
+require('UIColor,UIView,UILabel,UIFont');
 defineClass('ZLVIPCenterController', {
     viewDidLoad: function() {
         self.super().viewDidLoad();
@@ -9,6 +9,14 @@ defineClass('ZLVIPCenterController', {
         var view = UIView.alloc().initWithFrame(self.view().frame());
         view.setBackgroundColor(UIColor.whiteColor());
         self.view().addSubview(view);
+
+        var label = UILabel.alloc().initWithFrame(CGRectMake(10, 88,300, 20));
+        label.setText("会员中心全面升级中，敬请期待哦");
+        label.setTextColor(UIColor.darkGrayColor());
+        label.setTextAlignment(1);
+        label.setBackgroundColor(UIColor.clearColor());
+        label.setFont(UIFont.systemFontOfSize(13));
+        self.view().addSubview(label);
 
     },
     viewWillAppear: function(animated) {
