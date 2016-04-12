6d89dd70f195aee70746c8be3570ba70ae112273
liuchao <liuchao@imzhiliao.com>
2016-04-12 10:48:40 +0800

    Debug

--- iosfix.js
+++ iosfix.js
@@ -1,34 +1,13 @@
-require('UIColor,UIView,UILabel,UIButton,UIImage,UIFont,UIBarButtonItem');
-defineClass('ZLVIPCenterController', {
-    viewDidLoad: function() {
-        self.super().viewDidLoad();
 
-        self.setTitle("会员中心");
-        self.view().setBackgroundColor(UIColor.whiteColor());
-
-        var backButton = UIButton.alloc().initWithFrame({x:0, y:0, width:60, height:44});
-        backButton.setTitle_forState("返回", 0);
-        backButton.setTitleColor_forState(UIColor.darkGrayColor(), 0);
-        backButton.setImage_forState(UIImage.imageNamed("public_btn_back_black"), 0);
-        backButton.titleLabel().setFont(UIFont.systemFontOfSize(17));
-        backButton.addTarget_action_forControlEvents(self, "touchBackButton", 6);
-        var backBarButton = UIBarButtonItem.alloc().initWithCustomView(backButton);
-        self.navigationItem().setLeftBarButtonItem(backBarButton);
-
-    },
+defineClass('CreditStoreViewController', {
     viewWillAppear: function(animated) {
-        self.super().viewWillAppear(animated);
-
-        var view = UIView.alloc().initWithFrame(self.view().frame());
-        view.setBackgroundColor(UIColor.lightGrayColor());
-        self.view().addSubview(view);
-
-        var label = UILabel.alloc().initWithFrame(self.view().frame());
-        label.setText("会员中心全面升级中，敬请期待哦");
-        label.setTextColor(UIColor.darkGrayColor());
-        label.setTextAlignment(1);
-        label.setBackgroundColor(UIColor.clearColor());
-        label.setFont(UIFont.systemFontOfSize(13));
-        self.view().addSubview(label);
+        self.super().viewWillAppear(YES);
+        self.webView().setDelegate(self);
+        self.navigationController().setNavigationBarHidden_animated(YES, NO);
+    },
+    viewWillDisappear: function(animated) {
+        self.super().viewWillDisappear(YES);
+        self.webView().setDelegate(null);
+        self.navigationController().setNavigationBarHidden_animated(NO, NO);
     },
 });
