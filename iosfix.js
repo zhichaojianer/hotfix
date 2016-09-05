b6bbf13fc676829babe8067f3771985d24ebad48
zhichaojianer <zhichaojianer@163.com>
2016-09-05 10:36:52 +0800

    update
    test

--- iosfix.js
+++ iosfix.js
@@ -1,14 +1,5 @@
-require('NSString,ZLRichtxtInfo,NSURL');
-defineClass('NotifyCell', {
-    lectureShareViewTapAction: function(gestureRecognizer) {
-        var jsonObject = NSString.jsonObjectWithNSString(self.notifyMessage().messageInfo().shareJson());
-        var zlRichtxtInfo = ZLRichtxtInfo.getRichtxtInfoWithDict(jsonObject);
-        if (!NSString.checkNullNotContainsLineFeedString(jsonObject["contenturl"])) {
-            zlRichtxtInfo.setContenturl(NSURL.URLWithString(jsonObject["contenturl"].stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)));
-        } else if (!NSString.checkNullNotContainsLineFeedString(jsonObject["detailUrl"])) {
-            zlRichtxtInfo.setContenturl(NSURL.URLWithString(jsonObject["detailUrl"].stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)));
-            zlRichtxtInfo.setRichtxtType(1);
-        }
-        self.lectureShareBlock()(zlRichtxtInfo);
+defineClass('AppDelegate', {
+    checkVersion: function() {
+        self.checkVersionRequest();
     },
 });
