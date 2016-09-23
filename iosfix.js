ba83ebec20a904beade8d472d81b6b8f9cc2033a
zhichaojianer <zhichaojianer@163.com>
2016-09-23 10:20:12 +0800

    update

--- iosfix.js
+++ iosfix.js
@@ -7,17 +7,17 @@ defineClass('ZLChosenTableController', {
         self.ui__view().setFrame(CGRectMake(0, 0, width, height));
         self.ui__pageControl().setTransform(CGAffineTransformMakeScale(0.75, 0.75));
         self.ui__pageControl().setBackgroundColor(UIColor.clearColor());
-        if (_ViewModel.zlbannerList().count() < 1) {
+        if (self.ViewModel.zlbannerList().count() < 1) {
             self.ui__pageControl().setNumberOfPages(1);
         } else {
-            self.ui__pageControl().setNumberOfPages(_ViewModel.zlbannerList().count());
+            self.ui__pageControl().setNumberOfPages(self.ViewModel.zlbannerList().count());
         }
         self.ui__pageControl().setPageIndicatorTintColor(UIColor.colorWithWhite_alpha(1, 0.5));
         self.ui__pageControl().setCurrentPageIndicatorTintColor(UIColor.colorWithRed_green_blue_alpha(255 / 255. f(), 109 / 255. f(), 83 / 255. f(), 1));
-        self.ui__scrollView().setContentSize(CGSizeMake(width * _ViewModel.zlbannerList().count(), height));
+        self.ui__scrollView().setContentSize(CGSizeMake(width * self.ViewModel.zlbannerList().count(), height));
         self.ui__scrollView().setScrollsToTop(NO);
-        for (var i = 0; i < _ViewModel.zlbannerList().count(); i++) {
-            var model = _ViewModel.zlbannerList[i]();
+        for (var i = 0; i < self.ViewModel.zlbannerList().count(); i++) {
+            var model = self.ViewModel.zlbannerList[i]();
             var imageView = UIImageView.alloc().initWithFrame(CGRectMake(width * i, 0, width, height));
             imageView.yy__setImageWithURL_placeholder(model.image(), null);
             imageView.setBackgroundColor(ColorManager.placeholderBackgroundColor());
