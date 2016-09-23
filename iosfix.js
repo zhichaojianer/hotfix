de3636df1e0450ea08d64ab5fe3c9382d834cd9b
zhichaojianer <zhichaojianer@163.com>
2016-09-23 10:31:30 +0800

    update

--- iosfix.js
+++ iosfix.js
@@ -1,9 +1,6 @@
 require('UIScreen,UIColor,UIImageView,ColorManager,UITapGestureRecognizer');
 defineClass('ZLChosenTableController', {
     createTabBanner: function() {
-
-        var width = UIScreen.mainScreen().bounds().size().width();
-        var height = width * (150 / 375.0 f);
  
-    },
+    }
 });
