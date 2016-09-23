8ee4dc01d3e3bcb6c157e1f65c70aa5549db1137
zhichaojianer <zhichaojianer@163.com>
2016-09-23 10:29:23 +0800

    update

--- iosfix.js
+++ iosfix.js
@@ -16,19 +16,7 @@ defineClass('ZLChosenTableController', {
         self.ui__pageControl().setCurrentPageIndicatorTintColor(UIColor.colorWithRed_green_blue_alpha(255 / 255. f(), 109 / 255. f(), 83 / 255. f(), 1));
         self.ui__scrollView().setContentSize(CGSizeMake(width * self.ViewModel.zlbannerList().count(), height));
         self.ui__scrollView().setScrollsToTop(NO);
-        for (var i = 0; i < self.ViewModel.zlbannerList().count(); i++) {
-            var model = self.ViewModel.zlbannerList[i]();
-            var imageView = UIImageView.alloc().initWithFrame(CGRectMake(width * i, 0, width, height));
-            imageView.yy__setImageWithURL_placeholder(model.image(), null);
-            imageView.setBackgroundColor(ColorManager.placeholderBackgroundColor());
-            imageView.setClipsToBounds(YES);
-            imageView.setContentMode(UIViewContentModeScaleAspectFill);
-            var tap = UITapGestureRecognizer.alloc().initWithTarget_action(self, @selector(tapBannerAction: ));
-            imageView.addGestureRecognizer(tap);
-            tap.view().setTag(i + 1);
-            imageView.setUserInteractionEnabled(YES);
-            self.ui__scrollView().addSubview(imageView);
-        }
+ 
         self.setupTimer();
     },
 });
