99af2d84ce67d4a1e19da1aac57f6ce78366cc42
zhichaojianer <zhichaojianer@163.com>
2016-09-23 10:48:24 +0800

    update

--- iosfix.js
+++ iosfix.js
@@ -14,16 +14,7 @@ defineClass('ZLChosenTableController', {
         self.ui__pageControl().setCurrentPageIndicatorTintColor(UIColor.colorWithRed_green_blue_alpha(255 / 255. f(), 109 / 255. f(), 83 / 255. f(), 1));
         self.ui__scrollView().setContentSize(CGSizeMake(width * self.ViewModel.zlbannerList().count(), height));
         self.ui__scrollView().setScrollsToTop(NO);
-        
-        for (var i = 0; i < self.ViewModel.zlbannerList().count(); i++) {
-            var model = self.ViewModel.zlbannerList[i]();
-            var imageView = UIImageView.alloc().initWithFrame(CGRectMake(width * i, 0, width, height));
-            imageView.setClipsToBounds(YES);
-            imageView.setContentMode(2);
-            tap.view().setTag(i + 1);
-            imageView.setUserInteractionEnabled(YES);
-            self.ui__scrollView().addSubview(imageView);
-        }
+    
         self.setupTimer();
     }
 });
