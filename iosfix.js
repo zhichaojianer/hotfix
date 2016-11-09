35d8b7ab6dfb39b8089d7aae7b7dab86bacad6ed
zhichaojianer <zhichaojianer@163.com>
2016-11-09 20:12:22 +0800

    test

--- iosfix.js
+++ iosfix.js
@@ -2,7 +2,8 @@ require('NSURL,UIApplication');
 defineClass('ZLACameraPlayerViewController', {
     startPlayback: function() {
         var string = "http://imzhiliao.com/vedio.html";
-        string = string.stringByAppendingString(self.zlaMediaPlayerInfo().zlaSrc());
+        var zlaSrc = self.zlaMediaPlayerInfo().zlaSrc();
+        string = string.stringByAppendingString(zlaSrc);
         var videoUrl = NSURL.URLWithString(string);
         UIApplication.sharedApplication().openURL(videoUrl);
         self.navigationController().popViewControllerAnimated(YES);
