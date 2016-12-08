0bf89be6bb8aa238eece40be51f69d5dfed930bc
zhichaojianer <zhichaojianer@163.com>
2016-12-08 11:46:58 +0800

    test

--- iosfix.js
+++ iosfix.js
@@ -1,16 +1,10 @@
 require('ZLDefaults');
-defineClass('AppDelegate', {
-    checkVersion: function() {
+defineClass('ZLAboutUsViewController', {
+    verisonUpdate: function() {
+
         ZLDefaults.sharedInstance().setRedPointShow(NO);
 
-        _zlHomeViewModel.checkVersionWithComplete(block(function() {
+        self.showVersionUpdateAlertView();
 
-            if (ZLDefaults.sharedInstance().redPointShow()) {
-                _tabBarVC.tabBar().showRedPointOnItemIndex(3);
-            } else {
-                _tabBarVC.tabBar().hideRedPointOnItemIndex(3);
-            }
-            self.showVersionUpdateAlertView();
-        }));
     },
 });
